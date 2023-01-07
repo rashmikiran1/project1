f7e4480a645e43378d85a22bef1391ee439d7d38
rashmikiran1 <rashmikiran55@gmail.com>
2023-01-07 17:02:19 +0530

    tag added

--- dom.js
+++ dom.js
@@ -2,10 +2,19 @@ var title = document.getElementById("header-title");
 console.log(title);
 title.textContent = "hello"
 var list = document.getElementsByClassName("list-group-item");
-console.log(list[0]);
+//console.log(list[0]);
 list[3].style.backgroundColor = "green";
 list[0].style.fontWeight = "bold";
 list[1].style.fontWeight = "bold";
 list[2].style.fontWeight = "bold";
+
+var li = document.getElementsByClassName("list-item");
+li[0].style.backgroundColor = "red";
+
+var tag = document.getElementsByTagName("li");
+for(var j=0; j<tag.length; j++){
+    tag[j].style.backgroundColor = "violet";
+}
+
 
 
