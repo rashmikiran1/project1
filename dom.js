9e6c39aaff20d620dc1904fda150cdeafe844cd4
rashmikiran1 <rashmikiran55@gmail.com>
2023-01-07 16:28:56 +0530

    style added

--- dom.js
+++ dom.js
@@ -1,3 +1,11 @@
 var title = document.getElementById("header-title");
 console.log(title);
 title.textContent = "hello"
+var list = document.getElementsByClassName("list-group-item");
+console.log(list[0]);
+list[3].style.backgroundColor = "green";
+list[0].style.fontWeight = "bold";
+list[1].style.fontWeight = "bold";
+list[2].style.fontWeight = "bold";
+
+
