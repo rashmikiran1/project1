3e149bfd1ff13e44f4a88ecfe460c62367f99a89
rashmikiran1 <rashmikiran55@gmail.com>
2023-07-12 17:44:14 +0530

    react

---
+++ shaprpener/src/App.js
@@ -0,0 +1,10 @@
+import Expense from "./component/expense";
+function App() {
+  return (
+    <div>
+     <Expense></Expense> 
+    </div>
+  );
+}
+
+export default App;
---
+++ shaprpener/src/component/expense.js
@@ -0,0 +1,12 @@
+function Expense() {
+    return (
+      <div>
+       <h1>Expense items</h1> 
+       <h2>Food Rs.10</h2>
+       <h2>Petrol Rs.100</h2>
+       <h2>Movie Rs.200</h2>
+      </div>
+    );
+  }
+  
+  export default Expense;
