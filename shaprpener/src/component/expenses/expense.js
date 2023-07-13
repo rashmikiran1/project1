38ed2271604840d5ae5c31cdef02f213d55658a9
rashmikiran1 <rashmikiran55@gmail.com>
2023-07-13 16:56:09 +0530

    delete

--- shaprpener/src/component/expenses/expense.js
+++ shaprpener/src/component/expenses/expense.js
@@ -2,11 +2,14 @@ import './expense.css';
 import ExpenseDate from './ExpenseeDate';
 import ExpenseDetails from './ExpenseDetails';
 const Expense= (props) => {
-
+const clickHandler = () => {
+  console.log("clicked")
+}
     return (
       <div className="expend">
         <ExpenseDate date={props.date} />
         <ExpenseDetails title={props.title} location={props.location} amount={props.amount} />
+        <button onClick={clickHandler}>delete</button>
       </div>
     );
   }
