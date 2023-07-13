03c2f90ac0f70f75af7f9f2d3da35aa0b7de9f61
rashmikiran1 <rashmikiran55@gmail.com>
2023-07-14 00:44:02 +0530

    eventhandler

--- shaprpener/src/component/ExpenseForm.js
+++ shaprpener/src/component/ExpenseForm.js
@@ -1,22 +1,34 @@
+import { useState } from "react";
 import "../component/form.css";
 function ExpenseForm() {
+    const [title,setTitle] = useState('')
+    const [date,setDate] = useState('')
+    const [amount,setAmount] = useState('')
     const clickhandler = (event) => {
-     console.log(event.target.value)
+     setTitle(event.target.value)
     }
+    
+    const clickdatehandler = (event) => {
+        setDate(event.target.value)
+    }
+    
+    const clickamounthandler = (event) => {
+        setAmount(event.target.value)
+       }
     return (
             <form>
                 <div className="form">
                 <div className="formtitile">
                 <label>ExpenseTitle</label>
-                <input type="text" onChange={clickhandler} />
+                <input type="text" value={title} onChange={clickhandler} />
                 </div>
                 <div >
                 <label>ExpenseDate</label>
-                <input  type="date" onChange={clickhandler} />
+                <input  type="date" value={date} onChange={clickdatehandler} />
                 </div>
                 <div>
                 <label>ExpenseAmount</label>
-                <input type="number" onChange={clickhandler}/>
+                <input type="number" value={amount} onChange={clickamounthandler}/>
                 </div>
                 <button onChange={clickhandler}>ADD</button>
                 </div>
