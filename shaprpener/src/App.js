6866112de9dadb836ecb19f4014e375fa47a437a
rashmikiran1 <rashmikiran55@gmail.com>
2023-07-16 19:11:45 +0530

    toggle

--- shaprpener/src/App.js
+++ shaprpener/src/App.js
@@ -5,14 +5,23 @@ import ExpenseFilter from './component/expenses/ExpenseFilter';
 
 function App() {
   const [expenses, setExpenses] = useState([]);
-  const [filteredYear,setFilteredyear] = useState('2020')
-  const filterhandler = (selectedyear) => {
-     setFilteredyear(selectedyear)
-}
+  const [showForm, setShowForm] = useState(false);
+  const [filteredYear, setFilteredYear] = useState('2020');
+
+  const filterHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => [...prevExpenses, expense]);
+    setShowForm(false);
   };
+
+  const toggleFormHandler = () => {
+    setShowForm((prevShowForm) => !prevShowForm); // Toggle the showForm state on button click
+  };
+
+  // Filter the expenses based on the selected year
   const filteredExpenses = expenses.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
@@ -20,19 +29,23 @@ function App() {
   return (
     <div>
       <h1>Expense Tracker</h1>
-      <ExpenseForm onAddExpense={addExpenseHandler} />
-      <ExpenseFilter selected={filteredYear} onChangeFilter ={filterhandler} />
-      {filteredExpenses.length===0 && <p>no expenses found</p>}
-      {filteredExpenses.length > 0 && 
-      filteredExpenses.map((expense, index) => (
-        <Expense
-          key={index}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date} 
-          filteredYear={filteredYear}
-        />
-      ))}
+      {showForm ? (
+        <ExpenseForm onAddExpense={addExpenseHandler} />
+      ) : (
+        <button onClick={toggleFormHandler}>Add Expenses</button>
+      )}
+      {showForm && <ExpenseFilter selected={filteredYear} onChangeFilter={filterHandler} />}
+      {showForm && filteredExpenses.length === 0 && <p>No expenses found for the selected year.</p>}
+      {showForm &&
+        filteredExpenses.map((expense, index) => (
+          <Expense
+            key={index}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+            filteredYear={filteredYear}
+          />
+        ))}
     </div>
   );
 }
